6198321a94be73decf0d50700219991f3a986e60
zhangxuanru <1107099011@qq.com>
2018-05-23 14:11:27 +0800

    zhibo index html

--- resources/views/layouts/zhibo/footer.php
+++ resources/views/layouts/zhibo/footer.php
@@ -1,10 +1,5 @@
- <div class="footer">
-  <div class="links">
-    <a href="//www.zhibo8.cc/baocuo.htm" class="repweb">报错反馈</a>
-    <a href="//www.zhibo8.cc/" class="pcs">电脑版</a>
-  </div>
-  <div class="copyright">
-    直播吧是一个体育导航，所有直播和视频链接均由网友提供，并链接到其他网站播放，如有异议请与我们取得联系。
-  </div>
-  <div class="gotop">返回顶部</div>
+<div class="footer container">
+  <div id="links"> 合作网站: <a href="//m.zhibo8.cc/" title="直播吧手机版" target="_blank">手机直播吧</a>&nbsp;&nbsp; <a href="http://www.188hi.com/" target="_blank">188Hi网址导航</a>&nbsp;&nbsp; <a href="http://www.hao123.com/" target="_blank">Hao123网址导航</a>&nbsp;&nbsp; <a href="//www.zhibo8.cc/link.htm" target="_blank">进入友情链接</a> <br />
+    <a href="//www.zhibo8.cc/contact.htm" target="_blank"><font color="red">联系我们</font></a>&nbsp;&nbsp;&nbsp;&nbsp; <a href="//www.zhibo8.cc/mianze.htm" target="_blank">免责声明</a> <a href="//pl.zhibo8.cc/usercenter/web/userService.html" target="_blank">用户协议</a> <a href="//www.zhibo8.cc/web/privacyPolicyPc.html" target="_blank">隐私政策</a> <a href="//www.zhibo8.cc/baocuo.htm" target="_blank">报错反馈</a> <a href="//m.zhibo8.cc/feedback/" target="_blank">投诉反馈</a>&nbsp;&nbsp;<a href="http://report.12377.cn:13225/toreportinputNormal_anis.do" target="_blank" rel="external nofollow">网上有害信息举报专区</a></div>
+  <div id="copyright"><a href="/">直播吧</a>所有视频均链接到各大视频网站播放，本站不提供任何视听上传服务，如有异议请与我们取得联系。<br>闽网文（2016）5589-096号  闽B2-20160123  闽ICP备15018471号-2 <br><img src="//static4style.qiumibao.com/common/img/policy_icon.jpg" /> 闽公网安备 35020302001925号</div>
 </div>
--- resources/views/layouts/zhibo/head.php
+++ resources/views/layouts/zhibo/head.php
@@ -1,22 +1,18 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
-    <meta name="format-detection"content="telephone=no">
-    <meta name="apple-mobile-web-app-capable" content="no" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-    <title>直播吧手机版</title>
+    <meta name="renderer" content="webkit" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="mobile-agent" content="format=html5;url=http://m.zhibo8.cc/" />
+    <title>直播吧</title>
     <meta name="keywords" content="直播吧,NBA,足球,体育,直播8,zhibo8,zhiboba" />
-    <meta name="Description" content="直播吧是知名体育平台,为足球迷,篮球迷等体育用户提供直播,新闻资讯,数据,论坛社区,原创报道,视频集锦等服务" />
+    <meta name="Description" content="直播吧是知名体育平台,主要为足球迷,篮球迷等提供直播,新闻资讯,数据,论坛社区,原创报道,视频集锦等服务" />
+    <meta property="qh.webmaster" content="52f504981c456"/>
     <meta name="apple-itunes-app" content="app-id=1221949580" />
-    <meta name="baidu-tc-cerfication" content="af2b65cca836092742410cc25f50af21" />
-    <link rel="stylesheet" type="text/css" href="/static/zhibo8/css/style.css" />
-    <link rel="stylesheet" type="text/css" href="/static/zhibo8/css/page.css" />
-    <script type="text/javascript" name="baidu-tc-cerfication" data-appid="5350162" src="/static/zhibo8/js/lightapp.js"></script>
-    <script src="/static/zhibo8/js/jquery.min.js"></script>
-    <script src="/static/zhibo8/js/js/main.js"></script>
-    <script src="/static/zhibo8/js/jquery.lazyload.js"></script>
-    <script type="text/javascript" src="/static/zhibo8/js/w.js"></script>
+    <meta name="_globalsign-domain-verification" content="PPZfTtJvqdPkFgJVjpa151m3awmDFxNXhDQ5uw92l0" />
+    <script src="https://www.zhibo8.cc/js/mobile_jump.js" type="text/javascript"></script>
+    <script src="https://www.zhibo8.cc/js/ad.js" type="text/javascript"></script>
+    <link rel="stylesheet" type="text/css" href="https://www.zhibo8.cc/css/reset.css" />
+    <link rel="stylesheet" type="text/css" href="https://www.zhibo8.cc/css/v2/common.css" />
 </head>
