c1db1742f0dfb311a683ffd9f679cc69aef21db9
zhangxuanru <1107099011@qq.com>
2018-06-12 13:56:11 +0800

    update login html

--- resources/views/zhibo/user/login.php
+++ resources/views/zhibo/user/login.php
@@ -55,8 +55,8 @@
                 <div class="layui-form-item">
                     <label class="layui-form-label">验证码</label>
                     <div class="layui-input-inline">
-                        <input type="text" name="username" lay-verify="required" placeholder="请输入验证码" autocomplete="off" class="layui-input">
-                        <button class="layui-btn">获取验证码</button>
+                        <input type="text" name="username" lay-verify="required" placeholder="请输入验证码" autocomplete="off" class="layui-input" style="float: left;width: 53%">
+                        <button class="layui-btn" style="float: left">获取验证码</button>
                     </div>
                 </div>
                 <div class="layui-form-item">
