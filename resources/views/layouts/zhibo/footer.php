63751ca6cd5c7b143e716568e8b7500cf6acab2e
zhangxuanru <1107099011@qq.com>
2018-06-14 14:41:35 +0800

    update title

--- resources/views/layouts/zhibo/footer.php
+++ resources/views/layouts/zhibo/footer.php
@@ -1,8 +1,12 @@
 <div class="footer container">
   <div id="links"> 合作网站: <a href="//m.zhibo8.cc/" title="直播吧手机版" target="_blank">手机直播吧</a>&nbsp;&nbsp; <a href="http://www.188hi.com/" target="_blank">188Hi网址导航</a>&nbsp;&nbsp; <a href="http://www.hao123.com/" target="_blank">Hao123网址导航</a>&nbsp;&nbsp; <a href="//www.zhibo8.cc/link.htm" target="_blank">进入友情链接</a> <br />
     <a href="//www.zhibo8.cc/contact.htm" target="_blank"><font color="red">联系我们</font></a>&nbsp;&nbsp;&nbsp;&nbsp; <a href="//www.zhibo8.cc/mianze.htm" target="_blank">免责声明</a> <a href="//pl.zhibo8.cc/usercenter/web/userService.html" target="_blank">用户协议</a> <a href="//www.zhibo8.cc/web/privacyPolicyPc.html" target="_blank">隐私政策</a> <a href="//www.zhibo8.cc/baocuo.htm" target="_blank">报错反馈</a> <a href="//m.zhibo8.cc/feedback/" target="_blank">投诉反馈</a>&nbsp;&nbsp;<a href="http://report.12377.cn:13225/toreportinputNormal_anis.do" target="_blank" rel="external nofollow">网上有害信息举报专区</a></div>
-  <div id="copyright"><a href="/">直播吧</a>所有视频均链接到各大视频网站播放，本站不提供任何视听上传服务，如有异议请与我们取得联系。<br>闽网文（2016）5589-096号  闽B2-20160123  闽ICP备15018471号-2 <br><img src="//static4style.qiumibao.com/common/img/policy_icon.jpg" /> 闽公网安备 35020302001925号</div>
+  <div id="copyright"><a href="/">版本所有:直播吧</a>所有视频均链接到各大视频网站播放，本站不提供任何视听上传服务，如有异议请与我们取得联系。<br>闽网文（2016）5589-096号  闽B2-20160123  闽ICP备15018471号-2 <br><img src="//static4style.qiumibao.com/common/img/policy_icon.jpg" /> 闽公网安备 35020302001925号</div>
 </div>
 <script src="/static/zhibo8/js/jquery.min.js"></script>
 <script src="/static/zhibo8/js/common.js" type="text/javascript"></script>
 <link rel="stylesheet" type="text/css" href="/static/zhibo8/css/page.css" />
+
+<script type="text/javascript">
+  console.log("技术交流: \r\n github:https://github.com/zhangxuanru,\r\n mailbox:strive965432@gmail.com");
+</script>
