1ee8e74e8fd099e5908fc9cb3453bf47eda4726e
zhangxuanru <1107099011@qq.com>
2018-07-16 12:54:29 +0800

    update table entitle

--- app/Models/Entity/LiveTeamTable.php
+++ app/Models/Entity/LiveTeamTable.php
@@ -21,7 +21,7 @@ class LiveTeamTable extends Model
     /**
      * @var int $id 
      * @Id()
-     * @Column(name="id", type="tinyint")
+     * @Column(name="id", type="integer")
      */
     private $id;
 
